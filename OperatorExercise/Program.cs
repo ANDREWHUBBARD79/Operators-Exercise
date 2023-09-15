3ade000d57c20d1b801645efe29d48287692a408
Andrew <andrewhubbard78@gmail.com>
2023-09-15 18:02:06 -0400

    finished project

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // For division creat two integer variables called a and b. 
+
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a%b ;
+
+            Console.WriteLine(a+ " / " + b + " is "+quotient + " remainder "+remainder);
+            Console.WriteLine("Please enter the area of your circle:");
+            
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+            
         }
+        // define a method that perfomrs functionality that can be used over 
+        // and over
+
+        public static double AreaOfCircle(double radius)
+        {
+            // this is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+
+
+
     }
 }
